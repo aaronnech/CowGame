6ee5330e3ff326707f9c5703b4486bee096c45de
Ryan Drapeau <drapeau@cs.washington.edu>
2014-08-08 19:49:07 -0700

    states

--- js/model/markovchain.js
+++ js/model/markovchain.js
@@ -28,15 +28,6 @@ MarkovChain.prototype.getProbability = function(tailState, headState) {
     return this.transitionMatrix_[this.stateToIndex_[tailState]][this.stateToIndex_[headState]];
 };
 
-MarkovChain.prototype.getNeighborStates = function(state) {
-    var index = this.stateToIndex_[state];
-    var result = {"states" : []};
-    for (var i = 0; i < this.transitionMatrix_.length[index]; i++) {
-        result.states.push({"state" : this.indexToState_[i], "probability" : this.transitionMatrix_[index][i]});
-    }
-    return result;
-};
-
 MarkovChain.prototype.getNeighborProbabilities = function(state) {
     var index = this.stateToIndex_[state];
     var result = [];
